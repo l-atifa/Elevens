c3585a1cd0cd5b5dc5360c13f6814aaaf89b19d6
Latifa M <94848902+l-atifa@users.noreply.github.com>
2022-04-18 10:13:40 +0000

    activity 2

---
+++ Activity2StarterCode/Main.java
@@ -0,0 +1,16 @@
+/**
+ * This is a class that tests the Deck class.
+ */
+public class Main {
+
+	/**
+	 * The main method in this class checks the Deck operations for consistency.
+	 *	@param args is not used.
+	 */
+	public static void main(String[] args) 
+  {
+		ArrayList<Deck> d1 = new ArrayList<Deck>();
+    ArrayList<Deck> d2 = new ArrayList<Deck>();
+    ArrayList<Deck> d3 = new ArrayList<Deck>();
+	}
+}
